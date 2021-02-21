7bf242274bcf79949ccec09a1194763731e63163
Akash <akashkumar7005@gmail.com>
2021-02-22 00:28:55 +0530

    faqs

---
+++ src/components/Data.js
@@ -0,0 +1,23 @@
+export const Data = [
+
+    {
+        question: "Q1. Who is a Zomato Delivery Partner?",
+        answer:"Zomato Delivery Partners are independent contractors, who are required to pick up food or grocery items ordered by customers from restaurants / outlets and deliver to customers. Delivery Partners use their own vehicles (Bike/Cycle) to deliver orders to Zomato customers."
+    },
+    {
+        question: "Q2. What are the login hours?",
+        answer:"The login hours are flexible. You can choose to login anytime depending on your availability."
+    },
+    {
+        question: "Q3. When do I start delivering orders?",
+        answer:"Once your registration is complete, our team will run a background check after which you can start delivering orders."
+    },
+    {
+        question: "Q4. How much can I earn?",
+        answer:"You will be working on an earn per delivery model, you will earn for each delivery you make. For more information about this, please visit our nearest onboarding centre (or call on helpline number"
+    },  
+    {
+        question: "Q5. How will I get paid?",
+        answer:"We will transfer your earnings on a weekly basis directly in your bank account. You can check your earnings in your app under the Payments section."
+    }
+]
